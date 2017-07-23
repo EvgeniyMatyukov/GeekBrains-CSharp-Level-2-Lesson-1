6d6e25bc21a829fa59ed49e831143a1478691bfc
Evgeniy Matyukov <_x@bkru>
2017-07-23 21:10:20 +0400

    GeekBrains C# Level 2 Lesson 1 homework

---
+++ AsteroidGame/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace AsteroidGame
+{
+    static class Game
+    {
+        static BufferedGraphicsContext context;
+        static public BufferedGraphics buffer;
+        // Свойства
+        // Ширина и высота игрового поля
+        static public int Width { get; set; }
+        static public int Height { get; set; }
+        static Game()
+        {
+        }
+        static public void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // предоставляет доступ к главному буферу графического контекста для текущего приложения
+            context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();// Создаём объект - поверхность рисования и связываем его с формой
+                                      // Запоминаем размеры формы
+            Width = form.Width;
+            Height = form.Height;
+            // Связываем буфер в памяти с графическим объектом.
+            // для того, чтобы рисовать в буфере
+            buffer = context.Allocate(g, new Rectangle(0, 0, Width, Height));
+        }
+
+        static public void Draw()
+        {
+            // Проверяем вывод графики
+          //  buffer.Graphics.Clear(Color.Black);
+         //   buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+         //   buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            buffer.Render();
+        }
+    }
+}
+
